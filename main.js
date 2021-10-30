71913eef12034e9deb9457ff43eb5128a0452bce
Daniel Coker <danielcoker28@outlook.com>
2021-10-30 01:25:26 +0100

    Add update project functionality

--- main.js
+++ main.js
@@ -1,4 +1,68 @@
 /**
+ * Load project from the localStorage and update the projects select list.
+ */
+const loadProjects = () => {
+  const projectSelect = document.getElementById('projectList');
+  const projects = JSON.parse(localStorage.getItem('paystack-test-projects'));
+
+  projects.forEach((project) => {
+    const option = document.createElement('option');
+    option.value = project.paystackKey;
+    option.innerHTML = project.projectName;
+
+    projectSelect.appendChild(option);
+  });
+};
+
+/**
+ * Update the project form with the values from the localStorage.
+ * @param {Object} element The element object.
+ * @returns {Void}
+ */
+function updateProjectForm(element) {
+  const projectNameInput = element.options[element.selectedIndex].text;
+  const projects = JSON.parse(localStorage.getItem('paystack-test-projects'));
+
+  const project = projects.find(
+    (localProject) => localProject.projectName === projectNameInput
+  );
+
+  if (!project) {
+    return;
+  }
+
+  document.getElementById('projectId').value = project.projectId;
+  document.getElementById('projectName').value = project.projectName;
+  document.getElementById('paystackKey').value = project.paystackKey;
+  document.getElementById('reference').value = project.reference;
+  document.getElementById('verifyEndpoint').value = project.verifyEndpoint;
+  document.getElementById('authToken').value = project.authToken;
+}
+
+/**
+ * Update the project and save update to localStorage.
+ * @param {String} projectId The project ID.
+ * @param {Object} updatedValues The updated project values.
+ */
+const updateProject = (projectId, updatedValues) => {
+  const projects = JSON.parse(localStorage.getItem('paystack-test-projects'));
+
+  projects.find((localProject, i) => {
+    if (localProject.projectId === projectId) {
+      projects[i] = {
+        projectId,
+        ...updatedValues,
+      };
+      return true;
+    }
+  });
+
+  localStorage.setItem('paystack-test-projects', JSON.stringify(projects));
+
+  location.reload();
+};
+
+/**
  * Add a project to localStorage.
  * @param {Object} event The event object.
  * @returns {Void}
@@ -7,13 +71,27 @@ const addProject = (event) => {
   event.preventDefault();
 
   const projectForm = document.getElementById('addProjectForm');
+  const projectIdHiddenInput = document.getElementById('projectId').value;
 
-  const projectId = uuidv4();
   const projectName = document.getElementById('projectName').value;
   const paystackKey = document.getElementById('paystackKey').value;
   const reference = document.getElementById('reference').value;
   const verifyEndpoint = document.getElementById('verifyEndpoint').value;
   const authToken = document.getElementById('authToken').value;
+
+  // If the project has an already existing ID, update the project instead.
+  if (projectIdHiddenInput) {
+    updateProject(projectIdHiddenInput, {
+      projectName,
+      paystackKey,
+      reference,
+      verifyEndpoint,
+      authToken,
+    });
+    return;
+  }
+
+  const projectId = uuidv4();
 
   if (!projectName && !paystackKey) {
     console.log('Project name and paystack key are required.');
@@ -48,7 +126,7 @@ const addProject = (event) => {
 
   localStorage.setItem('paystack-test-projects', JSON.stringify(projects));
 
-  projectForm.reset();
+  location.reload();
 };
 
 /**
@@ -142,8 +220,14 @@ const payWithPaystack = (event) => {
 };
 
 const paymentForm = document.getElementById('paymentForm');
-
 const addProjectForm = document.getElementById('addProjectForm');
+// const projectList = document.getElementById('projectList');
+
+window.onload = () => {
+  loadProjects();
+};
+
+// projectList.onchange = updateProjectForm;
 
 paymentForm.addEventListener('submit', payWithPaystack, false);
 addProjectForm.addEventListener('submit', addProject, false);
